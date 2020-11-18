43724cdb4f8ddd5fde1325f8b856be3b4b85eff0
AleksandraZhi <jiterewa@gmail.com>
2020-11-18 13:53:38 +0000

    server setup

--- server/app.js
+++ server/app.js
@@ -9,7 +9,9 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use("/", routes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", routes)
 
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const mainController = require("../controllers/mainController");
+// const mainController = require("../controllers/mainController");
 
-router.get("/", mainController.novelsPage);
+// router.get("/", mainController.homePage);
 
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+// router.get("/", mainController.novelsPage);
+
+
+router.get("/novels-rockstar", (req, res) => {
+  res.render("layout");
+})
 
 module.exports = router;
