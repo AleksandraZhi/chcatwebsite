380e40ab148fb8932d153751ebc659f53a258413
AleksandraZhi <jiterewa@gmail.com>
2020-11-25 20:19:58 +0000

    remove unnecessary comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const mainController = require("../controllers/mainController");
-
-// router.get("/", mainController.homePage);
 
 router.get('/', (req, res) => {
   res.render('index');
 });
-
-// router.get("/", mainController.novelsPage);
-
 
 router.get("/novels-rockstar", (req, res) => {
   res.render("layout");
