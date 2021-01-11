76a197ee734527e4d914e9138b400f1fa3f34282
AleksandraZhi <jiterewa@gmail.com>
2021-01-11 20:31:46 +0000

    removed quotes when pulling info from mongodb

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const Book = mongoose.model('Book');
+const util = require('util');
 
 exports.mainPage = (req, res) => {
   res.render("../views/index");
@@ -10,8 +11,8 @@ exports.novelsPage = async (req, res) => {
   const text = await Book.findOne( {
     _id: "5ff898e92f8d9b2e3e677a2d"
   }).exec();
-  const bookTitle = JSON.stringify(text.bookTitle);
-  const bookChapter = JSON.stringify(text.bookChapter);
-  const bookText = JSON.stringify(text.bookText);
+  const bookTitle = util.format(text.bookTitle);
+  const bookChapter = util.format(text.bookChapter);
+  const bookText = util.format(text.bookText);
   res.render("../views/novels-main", {bookTitle, bookChapter, bookText});
 }
