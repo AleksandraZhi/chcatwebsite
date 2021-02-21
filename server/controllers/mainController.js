3a48aa73cf0030fa44c2c0a02ddbb8fcc8b80f5f
AleksandraZhi <jiterewa@gmail.com>
2021-02-21 21:02:05 +0000

    fixed back button to stay on the same chapter when it goes back on the top of the page

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -12,16 +12,18 @@ exports.mainPage = (req, res) => {
 
 
 exports.novelsPage = async (req, res) => {
-  const bookChapterNr =  req.params.chapterNr * 1;
+  try{const bookChapterNr =  req.params.chapterNr * 1;
   const text = await Novel.findOne( {
     bookTitle: "Рокзвезда",
     bookChapter: `${bookChapterNr}`
   }).exec();
-
+  const totalChapters = await Novel.countDocuments({bookTitle: "Рокзвезда"});
   const bookTitle = util.format(text.bookTitle);
   const bookChapter = util.format(text.bookChapter);
   const bookPath = util.format(text.bookPath);
   const bookText = fs.readFileSync(path.join(__dirname, bookPath), 'utf-8');
   const copyrightYear = year();
-  res.render('../views/novels-main', {bookTitle, bookChapter, bookText, copyrightYear});
+  res.render('../views/novels-main', {bookTitle, bookChapter, bookText, copyrightYear, totalChapters})
+}
+  catch {err => console.log(err)}
 }
